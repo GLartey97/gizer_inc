7c431cdf5a26075eab543475778e51795e60212a
Gabriel Lartey <gabriellartey16@gmail.com>
2023-03-08 04:41:41 +0000

    Added a text theme

---
+++ lib/src/utils/themes/texttheme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TTextTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: GoogleFonts.montserrat(
+      color: Colors.black87,
+      fontSize: 36.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      color: Colors.black54,
+      fontSize: 32.0,
+    ),
+    bodySmall: const TextStyle(color: Colors.black87),
+  );
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: GoogleFonts.montserrat(
+      color: Colors.white,
+      fontSize: 36.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      color: Colors.white70,
+      fontSize: 32.0,
+    ),
+    bodySmall: const TextStyle(color: Colors.white),
+  );
+}
--- lib/src/utils/themes/theme.dart
+++ lib/src/utils/themes/theme.dart
@@ -1,34 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:gizer_inc/src/utils/themes/texttheme.dart';
 
 class TAppTheme {
   static ThemeData lightTheme = ThemeData(
       brightness: Brightness.light,
-      textTheme: TextTheme(
-        headlineLarge: GoogleFonts.montserrat(
-          color: Colors.black87,
-          fontSize: 36.0,
-          fontWeight: FontWeight.bold,
-        ),
-        headlineMedium: GoogleFonts.poppins(
-          color: Colors.black54,
-          fontSize: 32.0,
-        ),
-        bodySmall: const TextStyle(color: Colors.black87),
-      ),
+      textTheme: TTextTheme.lightTextTheme,
       primarySwatch: Colors.orange);
   static ThemeData darkTheme = ThemeData(
-      brightness: Brightness.dark,
-      textTheme: TextTheme(
-        headlineLarge: GoogleFonts.montserrat(
-          color: Colors.white,
-          fontSize: 36.0,
-          fontWeight: FontWeight.bold,
-        ),
-        headlineMedium: GoogleFonts.poppins(
-          color: Colors.white70,
-          fontSize: 32.0,
-        ),
-        bodySmall: const TextStyle(color: Colors.white),
-      ));
+      brightness: Brightness.dark, textTheme: TTextTheme.darkTextTheme);
 }
